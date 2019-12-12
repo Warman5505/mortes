ecb2f5864b56a5f540a9d8df4171bf45d2fd51de
Warman5505 <45209681+Warman5505@users.noreply.github.com>
2019-12-11 22:43:43 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,13 @@
-function auth() {
-alert("Hi");
+function auth(form) {
+  var user = form.username.value;
+  var pass = form.pwd.value;
+  
+  var user1 = "admin"
+  var pass1 = "password"
+  
+  if(user == user1 && pass == pass1){
+    document.write("Sup");
+  } else {
+    alert("Nope");
+  }
 }
