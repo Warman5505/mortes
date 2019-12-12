d4ec9efc32878397a9ddb9ccc8013d47158a9333
Warman5505 <45209681+Warman5505@users.noreply.github.com>
2019-12-11 23:33:41 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,8 +2,8 @@ function auth(form) {
   var user = form.username.value;
   var pass = form.pwd.value;
   
-  var user1 = admin;
-  var pass1 = password;
+  var user1 = "admin";
+  var pass1 = "password";
   
   if(user == user1 && pass == pass1){
     document.write("Sup");
