cac85e216e33e6c6246e027fee76883bdcf15c89
Warman5505 <45209681+Warman5505@users.noreply.github.com>
2019-12-12 15:21:04 -0500

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,1 +1,11 @@
-echo ["hi"];
+<?php
+  if (isset($_POST['searchterm'])) {
+    echo $_POST['searchterm'];
+  }
+?>
+<form
+      action=""
+      method="post"
+      <input type="text" name="searchterm">
+      <input type="submit" value"Search">
+</form>
