7cb2fa6c882d77e79a5a423a62ff5f958ca651e8
Warman5505 <45209681+Warman5505@users.noreply.github.com>
2019-12-13 11:12:20 -0500

    Delete auth.php

--- auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if (isset($_POST['searchterm'])) {
-    echo $_POST['searchterm'];
-  }
-?>
-<form
-      <input type="text" name="searchterm">
-      <input type="submit" value"Search">
-</form>
